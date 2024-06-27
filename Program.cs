ebc37e7c89d4945ceafd733c9e7362373c4a5068
catherine-m-zhang <t-cazhang@microsoft.com>
2024-06-27 15:48:38 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,73 @@
+using Kusto.Data;
+using Kusto.Data.Common;
+using Kusto.Data.Net.Client;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Data;
+using SpanRetriever1;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/getspan/{requestId}", (string requestId) =>
+{
+    string clusterUri = "https://aoaiagents1.westus.kusto.windows.net/";
+    var kcsb = new KustoConnectionStringBuilder(clusterUri)
+        .WithAadUserPromptAuthentication();
+    var spanIds = new List<string>();
+
+    using (var kustoClient = KustoClientFactory.CreateCslQueryProvider(kcsb))
+    {
+        string database = "prod";
+
+        // First query to go from requestId -> traceId
+        string query = $@"
+            union *
+            | where TIMESTAMP > ago(2d)
+            | where ['http.request.header.apim_request_id'] == '{requestId}'";
+
+        using (var response = kustoClient.ExecuteQuery(database, query, null))
+        {
+            int columnTraceId = response.GetOrdinal("env_dt_traceId");
+            string traceId = null;
+            int count = 0;
+
+            while (response.Read())
+            {
+                count++;
+                traceId = response.GetString(columnTraceId);
+            }
+
+            if (count != 1)
+            {
+                Console.WriteLine($"Error: Expected exactly one traceId, but found {count}.");
+                return Results.BadRequest($"Error: Expected exactly one traceId, but found {count}.");
+            }
+
+            Console.WriteLine("TraceId: " + traceId);
+
+            // Second query to go from traceId -> span
+            string secondQuery = $@"
+                Span 
+                | where env_dt_traceId has '{traceId}'";
+
+            using (var secondResponse = kustoClient.ExecuteQuery(database, secondQuery, null))
+            {
+                int columnSpanId = secondResponse.GetOrdinal("env_dt_spanId");
+
+                while (secondResponse.Read())
+                {
+                    Console.WriteLine("SpanId - {0}", secondResponse.GetString(columnSpanId));
+                    var spanId = secondResponse.GetString(columnSpanId);
+                    spanIds.Add(spanId);
+                }
+            }
+        }
+    }
+    return Results.Ok(JsonConvert.SerializeObject(spanIds));
+});
+
+
+app.Run();
---
+++ SpanData.cs
@@ -0,0 +1,47 @@
+﻿namespace SpanRetriever1
+{
+    public class SpanData
+    {
+        // Identifiers
+        public byte[] TraceId { get; set; } = new byte[16];
+        public byte[] SpanId { get; set; } = new byte[8];
+        public byte[]? ParentSpanId { get; set; }
+
+        // Span Metadata
+        public string Name { get; set; } = string.Empty;
+        public int Kind { get; set; } // Use appropriate enum or mapping for SpanKind
+        public string? TraceState { get; set; }
+
+        // Timing Information
+        public DateTimeOffset StartTime { get; set; }
+        public DateTimeOffset EndTime { get; set; } // End time of the span
+        public TimeSpan Duration => EndTime - StartTime; // Duration is calculated
+
+        // Instrumentation Library
+        public string LibraryName { get; set; } = string.Empty;
+        public string? LibraryVersion { get; set; }
+
+        // Attributes
+        public List<KeyValuePair<string, object>> Attributes { get; set; } = new();
+
+        // Status
+        public SpanStatus Status { get; set; } = new SpanStatus();
+
+        // Resource
+        public Resource Resource { get; set; } = new Resource();
+
+        // Flags
+        public uint Flags { get; set; }
+    }
+
+    public class SpanStatus
+    {
+        public int StatusCode { get; set; } // Use appropriate enum or mapping for StatusCode
+        public string? Description { get; set; }
+    }
+
+    public class Resource
+    {
+        public List<KeyValuePair<string, object>> Attributes { get; set; } = new();
+    }
+}
